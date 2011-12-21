erikmuttersbach/PowerDNS-REST-Backend

commit 169dfaf8a8863f15b9e8a7981bd695eb99b40082

merge postresolve/nodata hooks code
git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2309 d19b8d6e-7fed-0310-83ef-9ca221ded41b

pdns/lua-pdns-recursor.cc
@@ -1,4 +1,6 @@
 #include "lua-pdns-recursor.hh"
+#include "syncres.hh"
+#include <boost/foreach.hpp>
 
 #if !defined(PDNS_ENABLE_LUA) && !defined(LIBDIR)
 
@@ -13,6 +15,17 @@ bool PowerDNSLua::nxdomain(const ComboAddress& remote,const ComboAddress& local,
 {
   return false;
 }
+
+bool PowerDNSLua::nodata(const ComboAddress& remote,const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, int& res, bool* variable)
+{
+  return false;
+}
+
+bool PowerDNSLua::postresolve(const ComboAddress& remote,const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, int& res, bool* variable)
+{
+  return false;
+}
+
 
 bool PowerDNSLua::preresolve(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, int& res, bool* variable)
 {
@@ -61,6 +74,40 @@ bool netmaskMatchTable(lua_State* lua, const std::string& ip)
       return true;
   }
   return false;
+}
+
+
+
+void pushResourceRecordsTable(lua_State* lua, const vector<DNSResourceRecord>& records)
+{  
+  // make a table of tables
+  lua_newtable(lua);
+  
+  int pos=0;
+  BOOST_FOREACH(const DNSResourceRecord& rr, records)
+  {
+    // row number, used by 'lua_settable' below
+    lua_pushnumber(lua, ++pos);
+    // "row" table
+    lua_newtable(lua);
+    
+    lua_pushstring(lua, rr.qname.c_str());
+    lua_setfield(lua, -2, "qname");  // pushes value at the top of the stack to the table immediately below that (-1 = top, -2 is below)
+    
+    lua_pushstring(lua, rr.content.c_str());
+    lua_setfield(lua, -2, "content");
+    
+    lua_pushnumber(lua, rr.qtype.getCode());
+    lua_setfield(lua, -2, "qtype");
+    
+    lua_pushnumber(lua, rr.ttl);
+    lua_setfield(lua, -2, "ttl");
+    
+    lua_pushnumber(lua, rr.d_place);
+    lua_setfield(lua, -2, "place");
+    
+    lua_settable(lua, -3); // pushes the table we just built into the master table at position pushed above
+  }
 }
 
 extern "C" {
@@ -118,6 +165,29 @@ int logLua(lua_State *lua)
 }
 }
 
+int getFakeAAAARecords(const std::string& qname, const std::string& prefix, vector<DNSResourceRecord>& ret)
+{
+  int rcode=directResolve(qname, QType(QType::A), 1, ret);
+  
+  ComboAddress prefixAddress(prefix);
+
+  BOOST_FOREACH(DNSResourceRecord& rr, ret)
+  {    
+    if(rr.qtype.getCode() == QType::A && rr.d_place==DNSResourceRecord::ANSWER) {
+      ComboAddress ipv4(rr.content);
+      uint32_t tmp;
+      memcpy((void*)&tmp, &ipv4.sin4.sin_addr.s_addr, 4);
+      // tmp=htonl(tmp);
+      memcpy(((char*)&prefixAddress.sin6.sin6_addr.s6_addr)+12, &tmp, 4);
+      rr.content = prefixAddress.toString();
+      rr.qtype = QType(QType::AAAA);
+    }
+  }
+  return rcode;
+}
+
+
+
 PowerDNSLua::PowerDNSLua(const std::string& fname)
 {
   d_lua = lua_open();
@@ -143,7 +213,6 @@ PowerDNSLua::PowerDNSLua(const std::string& fname)
 
   lua_pushcfunction(d_lua, setVariableLua);
   lua_setglobal(d_lua, "setvariable");
-
 
   lua_pushcfunction(d_lua, getLocalAddressLua);
   lua_setglobal(d_lua, "getlocaladdress");
@@ -252,6 +321,16 @@ bool PowerDNSLua::axfrfilter(const ComboAddress& remote, const string& zone, con
   return true;
 }
 
+bool PowerDNSLua::nodata(const ComboAddress& remote, const ComboAddress& local,const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, int& res, bool* variable)
+{
+  return passthrough("nodata", remote, local, query, qtype, ret, res, variable);
+}
+
+bool PowerDNSLua::postresolve(const ComboAddress& remote, const ComboAddress& local,const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, int& res, bool* variable)
+{
+  return passthrough("postresolve", remote, local, query, qtype, ret, res, variable);
+}
+
 bool PowerDNSLua::getFromTable(const std::string& key, std::string& value)
 {
   lua_pushstring(d_lua, key.c_str()); // 4 is now '1'
@@ -281,7 +360,6 @@ bool PowerDNSLua::getFromTable(const std::string& key, uint32_t& value)
   return ret;
 }
 
-
 bool PowerDNSLua::passthrough(const string& func, const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& ret, 
   int& res, bool* variable)
 {
@@ -299,8 +377,19 @@ bool PowerDNSLua::passthrough(const string& func, const ComboAddress& remote, co
   lua_pushstring(d_lua,  query.c_str() );
   lua_pushnumber(d_lua,  qtype.getCode() );
 
-  if(lua_pcall(d_lua,  3, 2, 0)) { // error 
-    string error=string("lua error in '"+func+"': ")+lua_tostring(d_lua, -1);
+  int extraParameter = 0;
+  if(!strcmp(func.c_str(),"nodata")) {
+    pushResourceRecordsTable(d_lua, ret);
+    extraParameter++;
+  }
+  else if(!strcmp(func.c_str(),"postresolve")) {
+    pushResourceRecordsTable(d_lua, ret);
+    lua_pushnumber(d_lua, res);
+    extraParameter+=2;
+  }
+
+  if(lua_pcall(d_lua,  3 + extraParameter, 3, 0)) { 
+    string error=string("lua error in '"+func+"' while processing query for '"+query+"|"+qtype.getName()+": ")+lua_tostring(d_lua, -1);
     lua_pop(d_lua, 1);
     throw runtime_error(error);
     return false;
@@ -308,10 +397,23 @@ bool PowerDNSLua::passthrough(const string& func, const ComboAddress& remote, co
   
   *variable |= d_variable;
   
+  
+  if(!lua_isnumber(d_lua, 1)) {
+    string tocall = lua_tostring(d_lua,1);
+    string luaqname = lua_tostring(d_lua,2);
+    string luaprefix = lua_tostring(d_lua, 3);
+    lua_pop(d_lua, 3);
+    // cerr<<"should call '"<<tocall<<"' to finish off"<<endl;
+    ret.clear();
+    res=getFakeAAAARecords(luaqname, luaprefix, ret);
+    return true;
+    // returned a followup 
+  }
+  
   int newres = (int)lua_tonumber(d_lua, 1); // new rcode
   if(newres < 0) {
     //    cerr << "handler did not handle"<<endl;
-    lua_pop(d_lua, 2);
+    lua_pop(d_lua, 3);
     return false;
   }
   res=newres;
@@ -332,6 +434,7 @@ bool PowerDNSLua::passthrough(const string& func, const ComboAddress& remote, co
 #else
   int tableLen = lua_objlen(d_lua, 2);
 #endif
+  // cerr<<"Got back "<<tableLen<< " answers from Lua"<<endl;
 
   for(int n=1; n < tableLen + 1; ++n) {
     lua_pushnumber(d_lua, n);
@@ -358,11 +461,11 @@ bool PowerDNSLua::passthrough(const string& func, const ComboAddress& remote, co
     /* removes 'value'; keeps 'key' for next iteration */
     lua_pop(d_lua, 1); // table
 
-    // cerr<<"Adding content '"<<rr.content<<"' with place "<<(int)rr.d_place<<" \n";
+    //    cerr<<"Adding content '"<<rr.content<<"' with place "<<(int)rr.d_place<<" \n";
     ret.push_back(rr);
   }
 
-  lua_pop(d_lua, 2);
+  lua_pop(d_lua, 3);
 
   return true;
 }

pdns/pdns_recursor.cc
@@ -453,7 +453,6 @@ static void writePid(void)
 typedef map<ComboAddress, uint32_t, ComboAddress::addressOnlyLessThan> tcpClientCounts_t;
 tcpClientCounts_t __thread* t_tcpClientCounts;
 
-
 TCPConnection::TCPConnection(int fd, const ComboAddress& addr) : d_remote(addr), d_fd(fd)
 { 
   ++s_currentConnections; 
@@ -527,13 +526,25 @@ void startDoResolve(void *p)
        res = sr.beginResolve(dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), dc->d_mdp.d_qclass, ret);
 
       if(t_pdl->get()) {
-        if(res == RCode::NXDomain)
+        if(res == RCode::NoError) {
+	  vector<DNSResourceRecord>::const_iterator i;
+	  for(i=ret.begin(); i!=ret.end(); ++i) 
+	      if(i->qtype.getCode() == dc->d_mdp.d_qtype && i->d_place == DNSResourceRecord::ANSWER)
+		break;
+	  if(i == ret.end())
+	    (*t_pdl)->nodata(dc->d_remote, g_listenSocketsAddresses[dc->d_socket], dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), ret, res, &variableAnswer);
+	}
+	else if(res == RCode::NXDomain)
           (*t_pdl)->nxdomain(dc->d_remote, g_listenSocketsAddresses[dc->d_socket], dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), ret, res, &variableAnswer);
       }
+      
+      (*t_pdl)->postresolve(dc->d_remote, g_listenSocketsAddresses[dc->d_socket], dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), ret, res, &variableAnswer);
     }
+
+
     
     uint32_t minTTL=std::numeric_limits<uint32_t>::max();
-    if(res<0) {
+    if(res < 0) {
       pw.getHeader()->rcode=RCode::ServFail;
       // no commit here, because no record
       g_stats.servFails++;
@@ -811,7 +822,7 @@ string* doProcessUDPQuestion(const std::string& question, const ComboAddress& fr
     uint32_t age;
     if(!SyncRes::s_nopacketcache && t_packetCache->getResponsePacket(question, g_now.tv_sec, &response, &age)) {
       if(!g_quiet)
-        L<<Logger::Error<<t_id<< " question answered from packet cache from "<<fromaddr.toString()<<endl;
+	L<<Logger::Error<<t_id<< " question answered from packet cache from "<<fromaddr.toString()<<endl;
 
       g_stats.packetCacheHits++;
       SyncRes::s_queries++;
@@ -1977,7 +1988,7 @@ int main(int argc, char **argv)
     ::arg().set("packetcache-servfail-ttl", "maximum number of seconds to keep a cached servfail entry in packetcache")="60";
     ::arg().set("server-id", "Returned when queried for 'server.id' TXT or NSID, defaults to hostname")="";
     ::arg().set("remotes-ringbuffer-entries", "maximum number of packets to store statistics for")="0";
-    ::arg().set("version-string", "string reported on version.pdns or version.bind")="PowerDNS Recursor "VERSION" $Id$";
+    ::arg().set("version-string", "string reported on version.pdns or version.bind")="PowerDNS Recursor "VERSION" $Id: pdns_recursor.cc 1734 2010-11-14 21:12:51Z ahu $";
     ::arg().set("allow-from", "If set, only allow these comma separated netmasks to recurse")=LOCAL_NETS;
     ::arg().set("allow-from-file", "If set, load allowed netmasks from this file")="";
     ::arg().set("entropy-source", "If set, read entropy from this file")="/dev/urandom";

pdns/docs/pdns.xml
@@ -11625,22 +11625,26 @@ Feb 10 14:16:03 stats: 125784 questions, 13971 cache entries, 309 negative entri
       </sect2>
       <sect2><title>Writing Lua PowerDNS Recursor scripts</title>
 	<para>
-	  Once a script is loaded, PowerDNS looks for two functions: <function>preresolve</function> and <function>nxdomain</function>. Either or both of these
-	  can be absent, in which case the corresponding functionality is disabled.
+	  Once a script is loaded, PowerDNS looks for several functions, as detailed below. All of these functions are optional.
 	</para>
 	<para>
-	  <function>preresolve</function> is called before any DNS resolution is attempted, and if this function indicates it, it can supply a direct answer to the 
+	  <function>preresolve ( remoteip, domain, qtype )</function> is called before any DNS resolution is attempted, and if this function indicates it, it can supply a direct answer to the 
 	  DNS query, overriding the internet. This is useful to combat botnets, or to disable domains unacceptable to an organization for whatever reason.
 	</para>
 	<para>
-	  <function>nxdomain</function> is called after the DNS resolution process has run its course, but ended in an 'NXDOMAIN' situation, indicating that the domain
+	  <function>postresolve ( remoteip, domain, qtype, records, origrcode )</function> is called right before returning a response to a client (and, unless <function>setvariable()</function> is called, to the packet cache too). It allows inspection and modification of almost any detail in the return packet. Available since 3.4.
+	</para>
+	<para>
+	  <function>function nxdomain ( remoteip, domain, qtype )</function> is called after the DNS resolution process has run its course, but ended in an 'NXDOMAIN' situation, indicating that the domain
 	  or the specific record does not exist. This can be used for various purposes.
 	</para>
-	
 	<para>
-	  Both functions are passed the IP address of the requestor, plus the name and type being requested. In return, these functions indicate if they
-	  have taken over the request, or want to let normal proceedings take their course. Beyond version 3.1.7, the IP address on which the question was received
-	  is inserted immediately after the IP address of the requestor - so in that case there are 4 parameters.
+	  <function>function nodata ( remoteip, domain, qtype, records )</function> is just like <function>nxdomain</function>, except it gets called when a domain exists, but the requested type does not. This is where one would implement DNS64. Available since 3.4.
+	</para>
+		
+	<para>
+	  All these functions are passed the IP address of the requestor, plus the name and type being requested. In return, these functions indicate if they
+	  have taken over the request, or want to let normal proceedings take their course.
 	</para>
 	<para>
 	  <warning>
@@ -11669,6 +11673,7 @@ function nxdomain ( ip, domain, qtype )
   if not matchnetmask(ip, "10.0.0.0/8", "192.168.0.0/16")  then return -1, ret end -- only interfere with local queries
   ret[1]={qtype=pdns.A, content="127.1.2.3"}    -- add IN A 127.1.2.3
   ret[2]={qtype=pdns.A, content="127.3.2.1"}    -- add IN A 127.3.2.1
+  setvariable()
   return 0, ret                 -- return no error, plus records
 end
 	  </screen>
@@ -11754,7 +11759,7 @@ end
 	</para>
 	<para>
 	  The function <function>matchnetmask(ip, netmask1, netmask2..)</function> (or <function>matchnetmask(ip, {netmask1, netmask2})</function>) is available to match incoming queries against
-	  a number of netmasks. If any of these matches, the function returns true.
+	  a number of netmasks. If any of these match, the function returns true.
 	</para>
 	<para>
 	  To log messages with the main PowerDNS Recursor process, use <function>pdnslog(message)</function>. Available since 3.2.
@@ -11764,6 +11769,9 @@ end
 	</para>
 	<para>
 	  To indicate that an answer should not be cached in the packet cache, use <function>setvariable()</function>. Available since 3.3.
+	</para>
+	<para>
+	  To get fake AAAA records for DNS64 usage, use <function>return "getFakeAAAARecords", domain, "fe80::21b:77ff:0:0"</function>. Available since 3.4.
 	</para>
       </sect2>
     </sect1>

pdns/lua-pdns-recursor.hh
@@ -14,6 +14,8 @@ public:
   bool preresolve(const ComboAddress& remote,const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
   bool nxdomain(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
   bool axfrfilter(const ComboAddress& remote, const string& zone, const DNSResourceRecord& in, vector<DNSResourceRecord>& out);
+  bool nodata(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
+  bool postresolve(const ComboAddress& remote, const ComboAddress& local, const string& query, const QType& qtype, vector<DNSResourceRecord>& res, int& ret, bool* variable);
   ComboAddress getLocal()
   {
     return d_local;

pdns/powerdns-example-script.lua
@@ -38,6 +38,7 @@ function nxdomain ( remoteip, domain, qtype )
 	if qtype ~= pdns.A then return -1, {} end  --  only A records
 	if not string.find(domain, "^www%.") then return -1, {} end  -- only things that start with www.
 	
+	setvariable()
 	if matchnetmask(remoteip, {"127.0.0.1/32", "10.1.0.0/16"}) 
 	then 
 		print "dealing"
@@ -66,3 +67,33 @@ function axfrfilter(remoteip, zone, qname, qtype, ttl, priority, content)
 	ret[2]={qname=qname, qtype=pdns.TXT, content=os.date("Retrieved at %Y-%m-%d %H:%M"), ttl=ttl}
 	return 0, ret
 end
+
+function nodata ( remoteip, domain, qtype, records )
+	print ("nodata called for: ", remoteip, getlocaladdress(), domain, qtype)
+	if qtype ~= pdns.AAAA then return -1, {} end  --  only AAAA records
+
+	setvariable()
+    return "getFakeAAAARecords", domain, "fe80::21b:77ff:0:0"
+end	
+
+-- records contains the entire packet, ready for your modifying pleasure
+function postresolve ( remoteip, domain, qtype, records, origrcode )
+	print ("postresolve called for: ", remoteip, getlocaladdress(), domain, qtype, origrcode)
+
+	for key,val in ipairs(records) 
+	do
+		if(val.content == '173.201.188.46' and val.qtype == pdns.A)
+		then
+			val.content = '127.0.0.1'
+			setvariable()
+		end
+		if val.qtype == pdns.A and matchnetmask(remoteip, "192.168.0.0/16") and matchnetmask(val.content, "85.17.219.0/24") 
+		then
+			val.content = string.gsub(val.content, "^85.17.219.", "192.168.219.", 1)
+			setvariable()
+		end
+		
+	--	print(val.content)
+	end
+	return origrcode, records
+end	

pdns/slavecommunicator.cc
@@ -437,3 +437,10 @@ void CommunicatorClass::slaveRefresh(PacketHandler *P)
   }
 }  
 
+// stub for PowerDNSLua linking
+int directResolve(const std::string& qname, const QType& qtype, int qclass, vector<DNSResourceRecord>& ret)
+{
+  return -1;
+}
+
+

pdns/syncres.cc
@@ -1206,3 +1206,16 @@ void SyncRes::addAuthorityRecords(const string& qname, vector<DNSResourceRecord>
     ret.push_back(ns);
   }
 }
+
+// used by PowerDNSLua
+int directResolve(const std::string& qname, const QType& qtype, int qclass, vector<DNSResourceRecord>& ret)
+{
+  struct timeval now;
+  gettimeofday(&now, 0);
+  
+  SyncRes sr(now);
+  
+  int res = sr.beginResolve(qname, QType(qtype), qclass, ret);
+  cerr<<"Result: "<<res<<endl;
+  return res;
+}

pdns/syncres.hh
@@ -533,6 +533,7 @@ typedef boost::function<void*(void)> pipefunc_t;
 void broadcastFunction(const pipefunc_t& func, bool skipSelf = false);
 void distributeAsyncFunction(const pipefunc_t& func);
 
+int directResolve(const std::string& qname, const QType& qtype, int qclass, vector<DNSResourceRecord>& ret);
 
 template<class T> T broadcastAccFunction(const boost::function<T*()>& func, bool skipSelf=false);
 

commit 67b778c1236be0d1086c133bbda4355edcaba943

Turns out r2311 enforced building without Lua - now with and without are possible; fix crash when Lua is not used (introduced in r2309); revert r2316 to allow building with Lua
git-svn-id: svn://svn.powerdns.com/pdns/trunk/pdns@2322 d19b8d6e-7fed-0310-83ef-9ca221ded41b

pdns/lua-pdns-recursor.cc
@@ -1,7 +1,8 @@
 #include "lua-pdns-recursor.hh"
+#include "syncres.hh"
 #include <boost/foreach.hpp>
 
-#if !defined(PDNS_ENABLE_LUA) || !defined(LIBDIR)
+#if !defined(PDNS_ENABLE_LUA)
 
 // stub implementation
 

pdns/pdns_recursor.cc
@@ -527,18 +527,18 @@ void startDoResolve(void *p)
 
       if(t_pdl->get()) {
         if(res == RCode::NoError) {
-	  vector<DNSResourceRecord>::const_iterator i;
-	  for(i=ret.begin(); i!=ret.end(); ++i) 
-	      if(i->qtype.getCode() == dc->d_mdp.d_qtype && i->d_place == DNSResourceRecord::ANSWER)
-		break;
-	  if(i == ret.end())
-	    (*t_pdl)->nodata(dc->d_remote, g_listenSocketsAddresses[dc->d_socket], dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), ret, res, &variableAnswer);
-	}
-	else if(res == RCode::NXDomain)
+      	  vector<DNSResourceRecord>::const_iterator i;
+      	  for(i=ret.begin(); i!=ret.end(); ++i) 
+    	      if(i->qtype.getCode() == dc->d_mdp.d_qtype && i->d_place == DNSResourceRecord::ANSWER)
+          		break;
+      	  if(i == ret.end())
+      	    (*t_pdl)->nodata(dc->d_remote, g_listenSocketsAddresses[dc->d_socket], dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), ret, res, &variableAnswer);
+      	}
+      	else if(res == RCode::NXDomain)
           (*t_pdl)->nxdomain(dc->d_remote, g_listenSocketsAddresses[dc->d_socket], dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), ret, res, &variableAnswer);
-      }
       
       (*t_pdl)->postresolve(dc->d_remote, g_listenSocketsAddresses[dc->d_socket], dc->d_mdp.d_qname, QType(dc->d_mdp.d_qtype), ret, res, &variableAnswer);
+      }
     }
 
 
@@ -1988,7 +1988,7 @@ int main(int argc, char **argv)
     ::arg().set("packetcache-servfail-ttl", "maximum number of seconds to keep a cached servfail entry in packetcache")="60";
     ::arg().set("server-id", "Returned when queried for 'server.id' TXT or NSID, defaults to hostname")="";
     ::arg().set("remotes-ringbuffer-entries", "maximum number of packets to store statistics for")="0";
-    ::arg().set("version-string", "string reported on version.pdns or version.bind")="PowerDNS Recursor "VERSION" $Id: pdns_recursor.cc 1734 2010-11-14 21:12:51Z ahu $";
+    ::arg().set("version-string", "string reported on version.pdns or version.bind")="PowerDNS Recursor "VERSION" $Id$";
     ::arg().set("allow-from", "If set, only allow these comma separated netmasks to recurse")=LOCAL_NETS;
     ::arg().set("allow-from-file", "If set, load allowed netmasks from this file")="";
     ::arg().set("entropy-source", "If set, read entropy from this file")="/dev/urandom";